2b6b1f3437bc060a65a82044deb99debcb6fcc55
Odutola Kazeem <kazmobileapp@gmail.com>
2025-06-06 23:01:20 +0100

    feat: add request logging middleware

---
+++ src/common/middleware/logger.middleware.ts
@@ -0,0 +1,10 @@
+import { RequestHandler } from 'express';
+
+export const requestLogger: RequestHandler = (req, res, next) => {
+  const start = Date.now();
+  res.on('finish', () => {
+    const ms = Date.now() - start;
+    console.log(`${req.method} ${req.originalUrl} ${res.statusCode} - ${ms}ms`);
+  });
+  next();
+};
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { policiesRouter } from './policies/policies.routes';
+import { requestLogger } from './common/middleware/logger.middleware';
 
 const app = express();
 app.use(express.json());
+app.use(requestLogger);
 
 app.get('/', (_req, res) => {
   res.send('Hello World!');
